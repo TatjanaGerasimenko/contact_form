dba775521cb8e0668c8831b29dad2727fd5c58c7
TatjanaGerasimenko <45108110+TatjanaGerasimenko@users.noreply.github.com>
2024-01-12 09:21:09 +0300

    Add files via upload

---
+++ email.php
@@ -0,0 +1,36 @@
+<?php
+$to = "t.v.gerasimenko@yandex.ru";
+  $subject = "Тема сообщения";
+  $message = "Message, сообщение!";
+  $headers = "Content-type: text/plain; charset=utf-8 \r\n";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+  if(isset($_POST["username"]){
+    $name 		=trim(strip_tags($_POST["username"]));
+  }
+  if(isset($_POST["usernumber"]))
+  {
+    $number 	= trim(strip_tags($_POST["usernumber"]));
+  }
+  if (isset( $_POST["question"])) {
+    $question  = trim(strip_tags($question));
+  }
+
+    $message = "<html>";
+      $message .= "<body>";
+      $message .= "Телефон: ".$number;
+      $message .= "<br />";
+      $message .= "Имя: ".$name;
+      $message .= "<br />";
+      $message .= "Вопрос: ".$question;
+      $message .= "</body>";
+    $message .= "</html>";
+
+
+    mail ($to, $subject, $message, $headers);
+}
+else
+{
+  exit;
+}
+?>
